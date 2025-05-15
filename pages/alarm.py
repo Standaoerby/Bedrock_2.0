9e8cf71d451579e6a4735cf9e2fefd17bb8e37e2
Standaoerby <stanislav.shchukin@gmail.com>
2025-05-15 06:10:22 +0100

    go

---
+++ pages/alarm.py
@@ -0,0 +1,4 @@
+from kivymd.uix.screen import MDScreen
+
+class AlarmScreen(MDScreen):
+    pass  # здесь будет логика Alarm (если понадобится)
---
+++ pages/home.py
@@ -0,0 +1,4 @@
+from kivymd.uix.screen import MDScreen
+
+class HomeScreen(MDScreen):
+    pass  # здесь будет логика Home (если понадобится)
